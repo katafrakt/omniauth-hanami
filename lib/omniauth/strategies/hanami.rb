ff95a63030a8343309a30913b6283c8d66000b33
Paweł Świątkowski <inquebrantable@gmail.com>
2017-03-17 02:07:13 +0100

    Make it work

--- lib/omniauth/strategies/hanami.rb
+++ lib/omniauth/strategies/hanami.rb
@@ -4,22 +4,29 @@ module OmniAuth
       include OmniAuth::Strategy
 
       option :auth_key, ->(params) { params['user']['email'] }
+      option :password_key, ->(params) { params['user']['password'] }
       option :repository, nil
       option :encryption, :bcrypt
       option :repository_method, :find_by_credentials
 
-      def request_phase
+      def callback_phase
         return fail!(:invalid_credentials) unless identity
-        env['omniauth.auth'] = identity.to_hash
-        redirect_to '/auth/hanami/callback'
+        #env['omniauth.auth'] = identity.to_hash
+        super
       end
 
       private
 
+      uid do
+        identity
+      end
+
       def identity
+        return @identity if @identity
         method = options.fetch(:repository_method)
         conditions = { auth_key: options.fetch(:auth_key).(request.params) }
-        @identity ||= repository.new.public_send(method, conditions, request['password'])
+        password = options.fetch(:password_key).(request.params)
+        @identity = repository.new.public_send(method, conditions, password)
       end
 
       def repository

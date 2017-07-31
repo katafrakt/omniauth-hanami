f0ee55474165ec9367f70a8d74f1901c3992dffa
Paweł Świątkowski <pawel.swiatkowski@boostcom.no>
2017-07-31 18:56:51 +0200

    Add tests for current user object

--- test/test.rb
+++ test/test.rb
@@ -19,6 +19,10 @@ class MyTest < MiniTest::Test
     { user: { email: 'test@user.com', password: 'abc123xd' } }
   end
 
+  def current_user
+    last_request.env['warden'].user
+  end
+
   def test_hello_world
     get '/'
     assert last_response.ok?
@@ -37,5 +41,16 @@ class MyTest < MiniTest::Test
     post url, correct_credentials
     assert_equal 302, last_response.status
     assert_equal '/', last_response.headers['Location']
+  end
+
+  def test_user_in_session
+    post url, correct_credentials
+    assert current_user
+    assert_equal 'test user', current_user.name
+  end
+
+  def test_nil_user_in_incorrect_session
+    post url, incorrect_credentials
+    refute current_user
   end
 end
